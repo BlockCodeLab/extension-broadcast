74b2766b5ecba7cdbb3e965896b7c92a120b5c7a
Yeqin Gong <gon@gyeq.in>
2024-10-23 22:46:59 +0800

    rename and rewrite description

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import featureImage from './feature.png';
 import iconImage from './icon.svg';
 
 export default {
-  name: 'Broadcast',
-  description: 'Connecting the world.',
+  name: 'ESPNOW Broadcast',
+  description: 'Broadcast the messages via the ESPNOW.',
   image: featureImage,
   icon: iconImage,
   tags: ['blocks', 'espnow'],
@@ -11,12 +11,12 @@ export default {
   // l10n
   translations: {
     en: {
-      name: 'Broadcast',
-      description: 'Connecting the world.',
+      name: 'ESPNOW Broadcast',
+      description: 'Broadcast the messages via the ESP-NOW.',
     },
     'zh-Hans': {
       name: '无线广播',
-      description: '连通你我他。',
+      description: '通过 ESP-NOW 广播消息。',
     },
   },
 };
